105ae897570b2203e0489bfe9278cfeafec4b3d0
aronnair <arunmakesarun@gmail.com>
2017-02-28 10:08:45 +0530

    Minor changes

--- app/src/main/java/personal/aron/devstuff/HomeActivity.java
+++ app/src/main/java/personal/aron/devstuff/HomeActivity.java
@@ -23,14 +23,16 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Home screen fab button -- we wont be using it as of now.
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setVisibility(View.GONE);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
