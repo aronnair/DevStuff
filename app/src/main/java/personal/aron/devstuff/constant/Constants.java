ff31c61b6ffab1c6cab758b4d130326b13d782a8
aronnair <arunmakesarun@gmail.com>
2017-02-28 23:56:28 +0530

    Minor changes

--- app/src/main/java/personal/aron/devstuff/constant/Constants.java
+++ app/src/main/java/personal/aron/devstuff/constant/Constants.java
@@ -12,9 +12,10 @@ import personal.aron.devstuff.dto.Module;
 public interface Constants {
 
     Module moduleMaps = new Module("Maps", "Google map integrations with geofencing & more...", "", "http://3.bp.blogspot.com/_ZaGO7GjCqAI/Rh47v0ZvaoI/AAAAAAAABjA/20hcUi_0XVo/s640/maps-building-layer.png");
-    Module moduleAnimations = new Module("Animations", "Animation is fun...", "", "https://s3.amazonaws.com/university-prod/uploads/attachments/354/original/motion.png?1444883111");
+    Module moduleAnimations = new Module("Animations", "Animation is fun...", "", "http://nubiz.in/images/services/animation/2d.png");
+    Module moduleSocial = new Module("Facebook", "Facebook, friends & feeds...", "", "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRskHQWUFMAPUBkpgda3yqil5ANVLFDOCfALh484lS2E44wzGQMuA");
 
-    public Module[] moduleList = {moduleMaps,moduleAnimations};
+    public Module[] moduleList = {moduleMaps,moduleAnimations,moduleSocial};
 
     public int numColumnsGridHome = 2;
 
